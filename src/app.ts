7f6ecdff2b067acbcf7724e92df38589b8d8e5b7
Sawankumar Pal <sawankumarpal710@gmail.com>
2023-12-26 01:04:32 +0530

    add logger

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
-import express from "express";
+import express from 'express';
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Welcome to Auth Service");
+app.get('/', (req, res) => {
+    res.send('Welcome to Auth Service');
 });
 
 export default app;
